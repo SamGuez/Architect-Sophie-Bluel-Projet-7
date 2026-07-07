d435af79c82b26f81af6cae8f875e9943ab88d1c
SamGuez <samguezcompte2@gmail.com>
2026-07-07 17:21:58 +0200

    creation modale

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -81,6 +81,58 @@ function setupFilters(works) {
     });
 }
 
+// ouverture modale
 
+const editButton = document.querySelector(".edit-btn");
+console.log("editButton =", editButton);
+
+editButton.addEventListener("click", () => {
+    console.log("clic sur modifier");
+    document.getElementById("modal").classList.remove("hidden");
+});
+
+
+// Fermeture modale
+const modal = document.getElementById("modal");
+const closeModal = document.querySelector(".close-modal");
+
+closeModal.addEventListener("click", () => {
+    modal.classList.add("hidden");
+});
+
+modal.addEventListener("click", (event) => {
+    if (event.target === modal) {
+        modal.classList.add("hidden");
+    }
+});
+
+// Affichage gallerie dans la modale
+function displayModalGallery(works) {
+    const modalWorks = document.getElementById("modal-works");
+    modalWorks.innerHTML = "";
+
+    works.forEach(work => {
+        const div = document.createElement("div");
+        div.classList.add("modal-work");
+
+        div.innerHTML = `
+            <img src="${work.imageUrl}" alt="${work.title}">
+            <span class="delete-icon"><i class="fa-solid fa-trash-can"></i></span>
+        `;
+
+        modalWorks.appendChild(div);
+    });
+}
+//passer à l'ecran ajoutez une photo
+document.getElementById("open-add-photo").addEventListener("click", () => {
+    document.getElementById("modal-gallery").classList.add("hidden");
+    document.getElementById("modal-add").classList.remove("hidden");
+});
+
+// revenir à l'ecran gallerie
+document.getElementById("back-to-gallery").addEventListener("click", () => {
+    document.getElementById("modal-add").classList.add("hidden");
+    document.getElementById("modal-gallery").classList.remove("hidden");
+});
 
 init();
